1893352a7fef66896902ec9d0c6ad3acd5f16875
张敏捷 <502982165@qq.com>
2019-04-19 15:24:41 +0800

    add

---
+++ js/25. ReverseNodesink-Group.js
@@ -0,0 +1,48 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+  let counter = 0, temp = [], a = new ListNode(0), b = a;
+  while (head) {
+    if (counter < k) {
+      temp.push(new ListNode(head.val))
+      counter++;
+      head = head.next;
+    } else {
+      for (let i = temp.length - 1; i >= 0; i--) {
+        b.next = temp[i]
+        b = b.next
+      }
+      temp = []
+      counter = 0;
+    }
+  }
+  if (temp.length < k) {
+    temp = temp.reverse()
+    for (let i = temp.length - 1; i >= 0; i--) {
+      b.next = temp[i]
+      b = b.next
+    }
+  } else {
+    for (let i = temp.length - 1; i >= 0; i--) {
+      b.next = temp[i]
+      b = b.next
+    }
+  }
+  return a.next
+};
+
+let node = new ListNode(1)
+node.next = new ListNode(2)
+// node.next.next = new ListNode(3)
+// node.next.next.next = new ListNode(4)
+// node.next.next.next.next = new ListNode(5)
+
+console.log(reverseKGroup(node, 2))
