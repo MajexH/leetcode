f11123fb81da584f95073b0cc2b44c438747c9ca
majexh <502982165@qq.com>
2019-04-23 18:13:57 +0800

    modify 147

--- js/147. InsertionSortList.js
+++ js/147. InsertionSortList.js
@@ -8,23 +8,39 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var insertionSortList = function(head) {
-  // pre 和 last 用来记录当前子列表的头和尾
   let affiliate = new ListNode(0), pre = affiliate, last = head
   affiliate.next = head;
-  while (pre.next) {
-    let start = pre.next
-    while (start !== last) {
-      if (last.next.val < start.val) {
-
+  while (true) {
+    if (!last) break
+    let item = pre.next, insert = last.next
+    if (!insert) break
+    while (item !== insert) {
+      // 如果item的值大于insert 意味着insert要插入到item前面
+      // 此时 如果插入了 则可以知道last肯定向后移动一位
+      if (item.val > insert.val) {
+        // 首先将insert拿出来
+        last.next = last.next.next;
+        // 讲insert插入item前面
+        pre.next = insert;
+        insert.next = item;
+        break;
       }
-      start = start.next
+      item = item.next
+      pre = pre.next
     }
+    // 如果没插入 需要将last后移一位
+    if (item === insert) {
+      last = last.next
+    }
+    // 重置pre指针
+    pre = affiliate
   }
   return affiliate.next;
 };
 
-let node = new ListNode(4)
-node.next = new ListNode(2)
-node.next.next = new ListNode(1)
+let node = new ListNode(-1)
+node.next = new ListNode(5)
 node.next.next = new ListNode(3)
+node.next.next.next = new ListNode(4)
+node.next.next.next.next = new ListNode(0)
 console.log(insertionSortList(node))
