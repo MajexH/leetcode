4ca9124d0240fa34b7e37af5fed899d29d3cca4f
MajexH <502982165@qq.com>
2019-08-22 11:54:22 +0800

    modify 55

--- js/55. JumpGame.js
+++ js/55. JumpGame.js
@@ -3,20 +3,48 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-  return recursion(nums, 0)
+  // 表示所有点都还没有被访问过
+  let memo = Array(nums.length).fill(-1)
+  return dp(nums, 0, memo)
 };
 
+/**
+ * 超时
+ * @param {number[]} nums 
+ * @param {number} start 
+ */
 function recursion(nums, start) {
-  if (start > nums.length - 1) return false
   if (start === nums.length - 1) return true
-  for (let i = start; i < nums.length; i++) {
-    let flag = false
-    for (let j = nums[start]; j >= 1; j--) {
-      if (recursion(nums, j + start)) {
-        flag = true
-        break
-      }
-    }
-    return flag
+  let far = Math.min(start + nums[start], nums.length - 1)
+  for (let j = far; j > start; j--) {
+    if (recursion(nums, j)) return true
   }
+  return false
 }
+
+/**
+ * 自顶向下的动态规划 通过memo来优化
+ * 其实际的想法就是 我一个点跳到下一个点 如果下一个点是无法到达的 我自己就无法达到 如果下一个点事可以达到的 那么我这个点就可以达到
+ * 如果这个点还没有被访问过 就去试试能不能达到
+ */
+function dp(nums, start, memo) {
+  if (start === nums.length - 1) {
+    memo[start] = true
+    return true
+  }
+  let far = Math.min(start + nums[start], nums.length - 1), flag = false
+  for (let j = far; j > start; j--) {
+    if (memo[j] === -1) {
+      // 递归求解
+      memo[j] = dp(nums, j, memo)
+    }
+    if (memo[j]) { 
+      flag = true
+      break
+    }
+  }
+  memo[start] = flag
+  return flag
+}
+
+console.log(canJump([2,3,1,1,4]))
