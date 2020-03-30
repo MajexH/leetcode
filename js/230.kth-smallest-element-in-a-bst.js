a9857c0423df4eb2dd1c61cc11253cf635a13300
majexh <a502982165@gmail.com>
2020-03-30 17:28:12 +0800

    add 230

--- js/230.kth-smallest-element-in-a-bst.js
+++ js/230.kth-smallest-element-in-a-bst.js
@@ -3,8 +3,6 @@
  *
  * [230] Kth Smallest Element in a BST
  */
-
-// @lc code=start
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -37,5 +35,28 @@ function getCount(root) {
   if (!root) return 0
   return getCount(root.left) + getCount(root.right) + 1
 }
+// @lc code=start
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+  // inorder travel
+  let stack = []
+  while (root || stack.length !== 0) {
+    while (root) {
+      stack.push(root)
+      root = root.left
+    }
+    if (stack.length !== 0) {
+      root = stack.pop()
+      k--
+      if (k === 0) return root.val
+      root = root.right
+    }
+  }
+  return null
+};
 // @lc code=end
 
