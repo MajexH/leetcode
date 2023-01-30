1a558b35abb5f5c6bd6ec3f151b4ce492365b861
majexh <a502982165@gmail.com>
2023-01-30 14:41:20 +0800

    modify: add 1669

---
+++ java/src/leetcode/editor/cn/MergeInBetweenLinkedLists1669.java
@@ -0,0 +1,98 @@
+//给你两个链表 list1 和 list2 ，它们包含的元素分别为 n 个和 m 个。 
+//
+// 请你将 list1 中下标从 a 到 b 的全部节点都删除，并将list2 接在被删除节点的位置。 
+//
+// 下图中蓝色边和节点展示了操作后的结果： 
+// 
+// 请你返回结果链表的头指针。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+//输出：[0,1,2,1000000,1000001,1000002,5]
+//解释：我们删除 list1 中下标为 3 和 4 的两个节点，并将 list2 接在该位置。上图中蓝色的边和节点为答案链表。
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,100
+//0003,1000004]
+//输出：[0,1,1000000,1000001,1000002,1000003,1000004,6]
+//解释：上图中蓝色的边和节点为答案链表。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 3 <= list1.length <= 10⁴ 
+// 1 <= a <= b < list1.length - 1 
+// 1 <= list2.length <= 10⁴ 
+// 
+//
+// Related Topics 链表 👍 80 👎 0
+
+
+package leetcode.editor.cn;
+
+public class MergeInBetweenLinkedLists1669 {
+    public static void main(String[] args) {
+        Solution solution = new MergeInBetweenLinkedLists1669().new Solution();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+            ListNode mv = list1, mv2 = list2;
+            ListNode first = null, second = null;
+
+            for (int i = 0; i <= b + 1; i++) {
+                if (i == a - 1) {
+                    first = mv;
+                }
+                if (i == b + 1) {
+                    second = mv;
+                }
+                mv = mv.next;
+            }
+
+            while (mv2.next != null) {
+                mv2 = mv2.next;
+            }
+
+            assert first != null;
+            first.next = list2;
+            mv2.next = second;
+
+            return list1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
